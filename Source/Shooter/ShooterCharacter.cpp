2faef2c63b6fd6583f5a66c9e0d1e0e587aadd61
Andrew <91674829+Giboson@users.noreply.github.com>
2021-12-24 04:19:25 +0500

    compiling:

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -15,6 +15,30 @@ AShooterCharacter::AShooterCharacter()
 void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay() Called!"));
+
+	//int myInt{ (int)42.54 };
+	int myInt{ 42 };
+	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
+
+	float myFloat{ 3.14159f };
+	UE_LOG(LogTemp, Warning, TEXT("float myFloat: %f"), myFloat);
+
+	double myDouble{ 0.000756 };
+	UE_LOG(LogTemp, Warning, TEXT("double myDouble: %lf"), myDouble);
+
+	char MyChar{ 'A' };
+	UE_LOG(LogTemp, Warning, TEXT("char MyChar: %c"), MyChar);
+
+	wchar_t wideChar{L'A'};
+	UE_LOG(LogTemp, Warning, TEXT("wchar_t wideChar: %lc"), wideChar);
+
+	bool myBool{true};
+	UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), myBool);
+
+	//UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f bool: %d"), myInt, myFloat, myBool);
+	UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f bool: %d"), myInt, myFloat, false);
 	
 }
 
